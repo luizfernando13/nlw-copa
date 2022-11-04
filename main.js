c2b1c558251bdf86aab98d064f425aae5ef0380d
Luiz Fernando <fabianafernando2016@gmail.com>
2022-11-04 15:51:38 -0300

    commit

--- main.js
+++ main.js
@@ -25,3 +25,38 @@ document.querySelector('#cards').innerHTML =
         createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
         createCard("28/11", "segunda", createGame("switzerland", "13:00", "brazil") + createGame("portugal", "16:00", "uruguay")) +
         createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))
+
+
+$(".colors").find("div").each(function() {
+    $(this).on("click", function() {
+        var color = $(this).attr("class");
+        var select = $(this).attr("select", true);
+        var cards = $('#cards').html(); 
+        var header = $(this).attr("data-header") ? $(this).attr("data-header") : 'header';
+        
+        $(".colors").find("div").removeAttr("select");
+        $(this).attr("select", true);
+        $("body").removeAttr("class").addClass(color);
+        $("#cards").html(cards);
+        $("header img").attr("src", './assets/img/'+header+'.svg').attr("style", "width: 9rem;");
+        
+        localStorage.setItem('color', `${color}`);
+        localStorage.setItem('header', `${header}`);
+        
+    })
+});
+        
+$(document).ready(function() {
+    var color = localStorage.getItem('color');
+    var header = localStorage.getItem('header');
+    var headImg = header ? ''+header+'' : 'header';
+
+    $(".colors").find("div[class='yellow']").attr("select", true);
+    $("body").removeAttr("class").addClass(color);
+    $("header img").attr("src", './assets/img/'+headImg+'.svg').attr("style", "width: 9rem;");
+    
+    if(color == color) {
+        $(".colors").find("div").removeAttr("select");
+        $(".colors").find("div[class='"+color+"']").attr("select", true)
+    }
+})
